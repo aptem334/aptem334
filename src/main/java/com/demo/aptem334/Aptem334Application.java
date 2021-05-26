4f81110ec42dbe896876745a5347ed0dca31fc3a
aalekseenko <aalekseenko@neoflex.ru>
2021-05-26 09:58:00 +0300

    Initial commit

---
+++ src/main/java/com/demo/aptem334/Aptem334Application.java
@@ -0,0 +1,13 @@
+package com.demo.aptem334;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aptem334Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aptem334Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/aptem334/Aptem334ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.aptem334;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aptem334ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
